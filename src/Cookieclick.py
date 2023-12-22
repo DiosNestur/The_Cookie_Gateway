bbf70ae2cb088193b9461fa0040498910329a26e
Nestur <nestor.tur.ntf@gmail.com>
2023-12-22 06:15:35 +0100

    Conseguimos hacer que se clique la galleta en un bucle infinito, habia problemas con la posicion fijada del clic, haciendo que el bucle se reseteara, perdiendo eficiencia en galletas

--- src/Cookieclick.py
+++ src/Cookieclick.py
@@ -3,14 +3,19 @@ import numpy as np
 import pyautogui
 import time
 import sys
+import logging
+
+
 
 def find_image(image_path, threshold=0.6):
     # Load the template image in color
     template = cv2.imread(image_path)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)  # Convert to RGB
     w, h = template.shape[1], template.shape[0]
-    clicked = False  # Add a flag to track whether a click has been made
-
+    missed_count = 0  # Counter for missed detections
+    tolerance = 10
+    fixed_center_point = None  # Initialize the fixed center point, we want this so it doesnt break by center missmatching
+    num_clicks = 1000000 # How many clicks for loop you want
 
     while True:
         # Capture the screen
@@ -21,26 +26,32 @@ def find_image(image_path, threshold=0.6):
         # Perform template matching 
         res = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
         loc = np.where(res >= threshold)
-        
-        # Check if the image is found and no click has been made yet
-        if not clicked and np.any(res >= threshold):
-            pt = np.unravel_index(np.argmax(res), res.shape)  # Find the highest correlation point
-            click_point = (pt[1] + w//2, pt[0] + h//2)  # Calculate the center point for the click
-            pyautogui.click(click_point)  # Perform the click
-            print("Image found and clicked at:", click_point)
-            clicked = True  # Set the flag to True after the click
 
-        if clicked:
-            # Optionally, add a delay after clicking if you want the script to keep running
-            time.sleep(2)  # Wait for 2 seconds (or however long you wish)
-            # If you want to exit after clicking, uncomment the next two lines
-            # cv2.destroyAllWindows()
-            # sys.exit()
+        # If the image is found for the first time, calculate and fix the center point
+        if np.any(res >= threshold) and fixed_center_point is None:
+            pt = np.unravel_index(np.argmax(res), res.shape)
+            fixed_center_point = (pt[1] + w//2, pt[0] + h//2)  # Calculate the center point
+            print("Fixed center point set at:", fixed_center_point)
+
+        # Perform a series of clicks at the fixed center point
+        if fixed_center_point:
+            for _ in range(num_clicks):
+                pyautogui.click(fixed_center_point)
+                print("Clicked at fixed point:", fixed_center_point)
+                # time.sleep(0)  # Short delay between clicks
+            missed_count = 0  # Reset the missed count after clicking
+        else:
+            missed_count += 1
+            print("Image not found on screen. Miss count:", missed_count)
+            if missed_count >= tolerance:
+                print("Image missed too many times. Exiting.")
+                break
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("Exiting...")
             break
 
-        time.sleep(0.5)
+        time.sleep(0.5)  # Check for the image every half second
 
 # Path to the image you are looking for
 image_path = 'Images\\Cookie.PNG'
